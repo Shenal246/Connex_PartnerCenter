1c97e91dd8994dc438d98d2982067c0cc6aeceb2
RivinduTharaka <rivindu.20220728@iit.ac.lk>
2024-07-09 16:32:30 +0530

    commit progress bar

--- src/App.js
+++ src/App.js
@@ -2,12 +2,18 @@ import logo from './logo.svg';
 import './App.css';
 import Navbar from './Components/Public/Navbar/Navbar';
 import Tpnt from './Components/Public/topnotch/Tpnt';
+import Progress from './Components/Public/Progress/Progress';
+import SmCard from './Components/Public/SmCard/SmCard';
 
 function App() {
   return (
     <>
     <Navbar/>  {/* nirosh - navigation bar */}
     <Tpnt/>
+    <SmCard width="150px" height="150px" topic="Total Partners" value="24" />
+  
+    <Progress width="300px" height="60px" topic="Total Partners" value={"69"}/>
+    
     </>
   );
 }
