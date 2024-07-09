828bc02eb8919972497d5b32c8834843045c43c0
Shenal Perera <pererashenal369@gmail.com>
2024-07-09 11:43:12 +0530

    ChartCard and Footer created

--- src/App.js
+++ src/App.js
@@ -1,18 +1,30 @@
 
-import './App.css';
-import SmCard from './Components/Public/SmCard/SmCard';
-import Navbar from './Components/Public/Navbar/Navbar';
+// import './App.css';
+// import SmCard from './Components/Public/SmCard/SmCard';
+// import Navbar from './Components/Public/Navbar/Navbar';
+// import ChartCard from './Components/Public/ChartCard/ChartCard';
+// import Footer from './Components/Public/Footer/Footer';
 
-function App() {
-  return (
-    <div className="App">
-      <Navbar/>  {/* nirosh - navigation bar */}
-      <SmCard width="150px" height="150px" topic="Total Partners" value="24" />
-      <SmCard width="150px" height="150px" topic="Total Products" value="3,420" />
-      <SmCard width="150px" height="150px" topic="Average Time" value="2m 34s" />
-      <SmCard width="150px" height="150px" topic="New Partner Requests" value="10" />
-    </div>
-  );
-}
+// function App() {
+//   const data = [
+//     ['Task', 'No'],
+//     ['Approved', 21],
+//     ['Pending', 4],
+//     ['Rejected', 2]
+//   ];
+//   return (
+//     <div className="App">
+//       <Navbar />  {/* nirosh - navigation bar */}
+//       <SmCard width="150px" height="150px" topic="Total Partners" value="24" />
+//       <SmCard width="150px" height="150px" topic="Total Products" value="3,420" />
+//       <SmCard width="150px" height="150px" topic="Average Time" value="2m 34s" />
+//       <SmCard width="150px" height="150px" topic="New Partner Requests" value="10" />
+//       <SmCard width="150px" height="150px" topic="New Partner Requests" value="10" />
+//       <ChartCard width="250px" height="250px" topic="Request Status" dataArray={data} />
+//       <Footer/>
 
-export default App;
+//     </div>
+//   );
+// }
+
+// export default App;
