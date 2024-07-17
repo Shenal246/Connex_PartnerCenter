8ad3fbce4875c916f905309451049214722d4402
RivinduTharaka <rivindu.20220728@iit.ac.lk>
2024-07-17 11:26:39 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import ChartCard from './Components/Public/ChartCard/ChartCard';
 import Footer from './Components/Public/Footer/Footer';
 import Tpnt from './Components/Public/topnotch/Tpnt';
 import Progress from './Components/Public/Progress/Progress';
+import Table from './Components/Public/Table/table';
 
 function App() {
   const data = [
@@ -14,6 +15,21 @@ function App() {
     ['Pending', 4],
     ['Rejected', 2]
   ];
+
+
+  const columns = [
+    { header: 'Name', accessor: 'name' },
+    { header: 'Age', accessor: 'age' },
+    { header: 'Email', accessor: 'email' },
+  ];
+  
+  const data1 = [
+    { name: 'John Doe', age: 28, email: 'john@example.com' },
+    { name: 'Jane Smith', age: 34, email: 'jane@example.com' },
+    { name: 'Sam Green', age: 22, email: 'sam@example.com' },
+  ];
+
+
   return (
     <>
       <div className='asdfghjk row'>
@@ -27,6 +43,7 @@ function App() {
         <SmCard width="16vh" height="16vh" topic="New Partner Requests" value="10" />
         <ChartCard width="25vh" height="25vh" topic="Request Status" dataArray={data} />
         <Progress width="30vh" height="7vh" topic="Total Partners" value={"69"} />
+        <Table columns={columns} data={data1} />
 
       </div>
       <Footer />
